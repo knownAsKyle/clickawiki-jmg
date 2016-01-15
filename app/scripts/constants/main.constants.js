10ba47335888891b2e71d5dca791caff073cd655
Tony Klose <anthony.klose@gmail.com>
2016-01-15 16:15:25 -0600

    added some styling, added Void as a type

--- app/scripts/constants/main.constants.js
+++ app/scripts/constants/main.constants.js
@@ -3,7 +3,7 @@
 		firebaseURL: "https://quicktest1.firebaseio.com/wiki",
 		headerTitle: "Clickawiki",
 		defaultDeleteMessage: "Are you sure you want to delete this? ",
-		types: ["ArrayList", "Boolean", "Integer", "Double", "Number", "Object", "String"],
+		types: ["ArrayList", "Boolean", "Integer", "Double", "Number", "Object", "String", "Void"],
 		auth: {
 			email: "admin@admin.com"
 		},
