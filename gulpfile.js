14aa40d0b0a6274f0d83157afd26d4ac161aab3e
Kyle <Kyle.Uhan@gmail.com>
2016-01-18 10:50:33 -0600

    updated gulpfile, now will watch html changes in scripts/directives/ .. and move it to assets/templates

--- gulpfile.js
+++ gulpfile.js
@@ -4,32 +4,42 @@ var concat = require('gulp-concat');
 
 gulp.task("watch", watch);
 gulp.task('compress', compress);
+gulp.task('moveHtml', moveHtml);
 
 var folders = ["modules", "constants", "factories", "controllers", "directives", "filters"];
 var watchGroupJS = formatWatchGroup(folders, "app/scripts/", "/*js");
 
 function formatWatchGroup(list, pathConst, extension) {
-	if (list && Array.isArray(list)) {
-		pathConst = pathConst || "";
-		extension = extension || "";
-		var newArr = [];
-		for (var item in list) {
-			newArr.push(pathConst + list[item] + extension);
-		}
-		return newArr;
-	}
-	return [];
+    if (list && Array.isArray(list)) {
+        pathConst = pathConst || "";
+        extension = extension || "";
+        var newArr = [];
+        for (var item in list) {
+            newArr.push(pathConst + list[item] + extension);
+        }
+        return newArr;
+    }
+    return [];
 }
 
 function watch() {
-	return gulp.watch(watchGroupJS, ['compress']);
+
+    gulp.watch(watchGroupJS, ['compress']);
+    gulp.watch("app/scripts/directives/*.html", ['moveHtml']);
+
 }
 
 function compress() {
-	return gulp.src(watchGroupJS)
-		.pipe(concat('script.js'))
-		.pipe(gulp.dest('dist'))
-		.pipe(uglify())
-		.pipe(concat('script.min.js'))
-		.pipe(gulp.dest('dist'));
+    return gulp.src(watchGroupJS)
+        .pipe(concat('script.js'))
+        .pipe(gulp.dest('dist'))
+        .pipe(uglify())
+        .pipe(concat('script.min.js'))
+        .pipe(gulp.dest('dist'));
+}
+
+function moveHtml(e) {
+    return gulp.src("app/scripts/directives/*.html")
+        .pipe(gulp.dest('assets/templates'))
+
 }
