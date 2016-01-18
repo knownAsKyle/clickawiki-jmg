9ce116f5e08a29e34e436bfd751256df060ed924
Kyle <Kyle.Uhan@gmail.com>
2016-01-18 10:27:29 -0600

    save to pull

--- dist/script.js
+++ dist/script.js
@@ -2,38 +2,41 @@
     angular.module("clickawiki", []);
 })();
 (function() {
-	angular.module("clickawiki").constant("constants", {
-		///firebaseURL: "https://quicktest1.firebaseio.com/wiki",
-		firebaseURL: "https://apiwiki.firebaseio.com",
-		headerTitle: "Clickawiki",
-		defaultDeleteMessage: "Are you sure you want to delete this? ",
-		types: ["ArrayList", "Boolean", "Integer", "Double", "Number", "Object", "String", "Void"],
-		auth: {
-			email: "admin@admin.com"
-		},
-		popUpDeleteSettings: {
-			title: "Are you sure?",
-			text: "",
-			type: "warning",
-			showCancelButton: true,
-			confirmButtonColor: "#5cb85c",
-			confirmButtonText: "Yes, delete it!",
-			cancelButtonText: "No, cancel!",
-			closeOnConfirm: false,
-			closeOnCancel: false
-		},
-		loginPromptSettings: {
-			title: "Login",
-			text: "Please provide your access code:",
-			type: "input",
-			inputType: "password",
-			showCancelButton: true,
-			closeOnConfirm: false,
-			animation: "slide-from-top",
-			inputValue: "click",
-			inputPlaceholder: "Acess code..."
-		}
-	});
+    angular.module("clickawiki").constant("constants", {
+        ///firebaseURL: "https://quicktest1.firebaseio.com/wiki",
+        firebaseURL: "https://apiwiki.firebaseio.com",
+        headerTitle: "Clickawiki",
+        paths: {
+            templatePath: "assest/templates"
+        },
+        defaultDeleteMessage: "Are you sure you want to delete this? ",
+        types: ["ArrayList", "Boolean", "Integer", "Double", "Number", "Object", "String", "Void"],
+        auth: {
+            email: "admin@admin.com"
+        },
+        popUpDeleteSettings: {
+            title: "Are you sure?",
+            text: "",
+            type: "warning",
+            showCancelButton: true,
+            confirmButtonColor: "#5cb85c",
+            confirmButtonText: "Yes, delete it!",
+            cancelButtonText: "No, cancel!",
+            closeOnConfirm: false,
+            closeOnCancel: false
+        },
+        loginPromptSettings: {
+            title: "Login",
+            text: "Please provide your access code:",
+            type: "input",
+            inputType: "password",
+            showCancelButton: true,
+            closeOnConfirm: false,
+            animation: "slide-from-top",
+            inputValue: "click",
+            inputPlaceholder: "Acess code..."
+        }
+    });
 })();
 (function() {
     angular.module("clickawiki").factory("authFactory", authFactory);
@@ -415,55 +418,55 @@
     }
 })();
 (function() {
-	angular.module("clickawiki").directive("cwHeader", cwHeader);
-	cwHeader.$inject = ["$compile"];
+    angular.module("clickawiki").directive("cwHeader", cwHeader);
+    cwHeader.$inject = ["$compile"];
 
-	function cwHeader($compile) {
-		var template = [
-			'<nav class="navbar navbar-default">',
-			'	<div class="container">',
-			'		<div class="navbar-header">',
-			'			<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navcol-1" aria-expanded="false">',
-			'				<span class="sr-only">Toggle navigation</span>',
-			'				<span class="icon-bar"></span>',
-			'				<span class="icon-bar"></span>',
-			'				<span class="icon-bar"></span>',
-			'			</button>',
-			'           <a class="navbar-brand navbar-link" href="#">{{vm.headerTitle}}</a>',
-			'		</div>',
-			'		<div class="collapse navbar-collapse" id="navcol-1">',
-			'			<form class="navbar-form navbar-left" role="search">',
-			'				<div class="form-group">',
-			'					<div class="input-group">',
-			'						<input type="text" class="form-control" placeholder="Search" ng-model="vm.searchTerm">',
-			'						<span class="input-group-btn">',
-			'							<button class="btn btn-default" type="button"><span class="glyphicon glyphicon-search" aria-hidden="true" ng-click="vm.search(vm.searchTerm)"></span></button>',
-			'						</span>',
-			'					</div>',
-			'				</div>',
-			'			</form>',
-			'			<ul class="nav navbar-nav">',
-			'				<li ng-if="!vm.isLoggedIn"><a href="#" ng-click="vm.loginPrompt($event)">Log In</a></li>',
-			'				<li ng-if="vm.isLoggedIn"><a href="#" ng-click="vm.logOut($event)">Log Out</a></li>',
-			'			</ul>',
-			'		</div>',
-			'	</div>',
-			'</nav>'
-		].join('');
-		var directive = {
-			restrict: "EA",
-			transclude: true,
-		};
-		var templateUrl = null;
-		//templateUrl = "cwHeader.directive.html";
-		if (templateUrl) {
-			directive.templateUrl = templateUrl
-		} else {
-			directive.template = template;
-		}
+    function cwHeader($compile) {
+        var template = [
+            '<nav class="navbar navbar-default">',
+            '	<div class="container">',
+            '		<div class="navbar-header">',
+            '			<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navcol-1" aria-expanded="false">',
+            '				<span class="sr-only">Toggle navigation</span>',
+            '				<span class="icon-bar"></span>',
+            '				<span class="icon-bar"></span>',
+            '				<span class="icon-bar"></span>',
+            '			</button>',
+            '           <a class="navbar-brand navbar-link" href="#">{{vm.headerTitle}}</a>',
+            '		</div>',
+            '		<div class="collapse navbar-collapse" id="navcol-1">',
+            '			<form class="navbar-form navbar-left" role="search">',
+            '				<div class="form-group">',
+            '					<div class="input-group">',
+            '						<input type="text" class="form-control" placeholder="Search" ng-model="vm.searchTerm">',
+            '						<span class="input-group-btn">',
+            '							<button class="btn btn-default" type="button"><span class="glyphicon glyphicon-search" aria-hidden="true" ng-click="vm.search(vm.searchTerm)"></span></button>',
+            '						</span>',
+            '					</div>',
+            '				</div>',
+            '			</form>',
+            '			<ul class="nav navbar-nav">',
+            '				<li ng-if="!vm.isLoggedIn"><a href="#" ng-click="vm.loginPrompt($event)">Log In</a></li>',
+            '				<li ng-if="vm.isLoggedIn"><a href="#" ng-click="vm.logOut($event)">Log Out</a></li>',
+            '			</ul>',
+            '		</div>',
+            '	</div>',
+            '</nav>'
+        ].join('');
+        var directive = {
+            restrict: "EA",
+            transclude: true,
+        };
+        var templateUrl = null;
+        //templateUrl = "cwHeader.directive.html";
+        if (templateUrl) {
+            directive.templateUrl = templateUrl;
+        } else {
+            directive.template = template;
+        }
 
-		return directive;
-	}
+        return directive;
+    }
 })();
 (function() {
 	angular.module("clickawiki").filter("methodFilter", methodFilter);
