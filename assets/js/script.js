f202f8219a105419136c2d75108266758cfb6104
Kyle <Kyle.Uhan@gmail.com>
2016-01-19 13:55:37 -0600

    wired up dropdown return type option

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,3 @@
 (function() {
-	$('#method-return-types').on('click', 'li a', function(){
-		console.log('Clicked!');
-      $('#method-return-types button').html($(this).text() + ' <span class="caret"></span>');
-      $('#method-return-types button').val($(this).text());
-   });
+
 })();
